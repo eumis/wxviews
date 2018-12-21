279a5ec413f1422a6efd4c8a1e17ebe8f440da39
eumis <misievich@gmail.com>
2018-12-21 14:53:40 +0300

    removed call node

--- wxviews/__init__.py
+++ wxviews/__init__.py
@@ -1,5 +1,6 @@
 '''Package adapts pyviews for using with wxpython'''
 
+from pyviews.rendering.modifiers import import_global, inject_global, set_global
+from pyviews import Code
 from wxviews.core.containers import Container, View, For, If
 from wxviews.core.sizers import GrowableCol, GrowableRow
-from pyviews.rendering.modifiers import import_global, inject_global
