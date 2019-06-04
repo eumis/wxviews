3d3b42999fbc1759cbdc0e3a12078c510422cea6
eumis <misievich@gmail.com>
2019-06-04 16:35:38 +0300

    call modifier imported in root

--- wxviews/__init__.py
+++ wxviews/__init__.py
@@ -1,10 +1,11 @@
 """Package adapts pyviews for using with wxpython"""
 
-from pyviews.rendering import import_global, inject_global, set_global
+from pyviews.rendering.modifiers import import_global, inject_global, set_global, call
 from pyviews.code import Code
+
 from wxviews.binding import bind
 from wxviews.containers import Container, View, For, If
 from wxviews.sizers import GrowableCol, GrowableRow, sizer
 from wxviews.styles import Style, StylesView, style
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
